1297bff5154f64c4a34518df1937c849dcb8bb79
Tássio Nascimento Santos <tassiosantos@gmail.com>
2020-03-26 16:02:54 -0300

    arquivos novos

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,10 @@ Route::get('/fornecedores', 'FornecedorController@index');
 Route::get('/fornecedores/create', 'FornecedorController@create');
 
 Route::post('/fornecedores/store', 'FornecedorController@store');
+
+
+Route::get('api/clientes', 'FornecedorController@index');
+
+Route::get('api/cliente/create', 'FornecedorController@create');
+
+Route::post('api/cliente/store', 'FornecedorController@store');
