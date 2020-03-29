10b2cefaacba001ec22a4cf3b1228bde425e74f9
alvarosw <alvarossmiguel@gmail.com>
2020-03-29 13:47:31 -0300

    cliente completo

--- routes/web.php
+++ routes/web.php
@@ -12,31 +12,14 @@ Route::group(['prefix'=>'fornecedores'], function() {
     Route::get('create', 'FornecedorController@create');
 
     Route::post('store', 'FornecedorController@store');
+    Route::get('{id}/edit', 'FornecedorController@edit');
 
-<<<<<<< HEAD
-Route::post('/fornecedores/store', 'FornecedorController@store');
-
+    Route::put('{id}/update', 'FornecedorController@update');
+});
 
 Route::get('api/clientes', 'FornecedorController@index');
 
 Route::get('api/cliente/create', 'FornecedorController@create');
 
 Route::post('api/cliente/store', 'FornecedorController@store');
-=======
-    Route::get('{id}/edit', 'FornecedorController@edit');
 
-    Route::put('{id}/update', 'FornecedorController@update');
-});
-
-Route::group(['prefix'=>'clientes'], function() {
-    Route::get('', 'ClienteController@index');
-    
-    Route::get('create', 'ClienteController@create');
-
-    Route::post('store', 'ClienteController@store');
-
-    Route::get('{id}/edit', 'ClienteController@edit');
-
-    Route::put('{id}/update', 'ClienteController@update');
-});
->>>>>>> f8cdba0359f0ff594225d69d1482af880a5ce994
