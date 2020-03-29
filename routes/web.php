ad2ef5992db2f5cc887c1550bc3de01692df8561
alvarosw <alvarossmiguel@gmail.com>
2020-03-29 13:17:11 -0300

    ajuste na rota

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,7 @@ Route::group(['prefix'=>'fornecedores'], function() {
     Route::get('create', 'FornecedorController@create');
 
     Route::post('store', 'FornecedorController@store');
-
-<<<<<<< HEAD
-Route::post('/fornecedores/store', 'FornecedorController@store');
+});
 
 
 Route::get('api/clientes', 'FornecedorController@index');
@@ -22,21 +20,3 @@ Route::get('api/clientes', 'FornecedorController@index');
 Route::get('api/cliente/create', 'FornecedorController@create');
 
 Route::post('api/cliente/store', 'FornecedorController@store');
-=======
-    Route::get('{id}/edit', 'FornecedorController@edit');
-
-    Route::put('{id}/update', 'FornecedorController@update');
-});
-
-Route::group(['prefix'=>'clientes'], function() {
-    Route::get('', 'ClienteController@index');
-    
-    Route::get('create', 'ClienteController@create');
-
-    Route::post('store', 'ClienteController@store');
-
-    Route::get('{id}/edit', 'ClienteController@edit');
-
-    Route::put('{id}/update', 'ClienteController@update');
-});
->>>>>>> f8cdba0359f0ff594225d69d1482af880a5ce994
